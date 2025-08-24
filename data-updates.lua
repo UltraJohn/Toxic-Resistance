61be3293e4b022dade5d7390f7944d387f36ab56
John <johnh.dk93@gmail.com>
2025-08-24 20:42:58 +0200

    Initial Release.

--- data-updates.lua
+++ data-updates.lua
@@ -1,4 +1,4 @@
-for name, armor in pairs(data.raw.armor) do
+for name, armor in pairs(data.raw["armor"]) do
 	for _, resistance in pairs (armor.resistances or {}) do
 		if resistance.type == "acid" then
 			table.insert(armor.resistances, {type = "toxic", decrease = resistance.decrease, percent = resistance.percent})
@@ -6,7 +6,7 @@ for name, armor in pairs(data.raw.armor) do
 	end
 end
 
-for name, car in pairs(data.raw.car) do
+for name, car in pairs(data.raw["car"]) do
 	for _, resistance in pairs (car.resistances or {}) do
 		if resistance.type == "acid" then
 			table.insert(car.resistances, {type = "toxic", decrease = resistance.decrease, percent = resistance.percent})
@@ -15,6 +15,14 @@ for name, car in pairs(data.raw.car) do
 end
 
 for name, spidertron in pairs(data.raw["spider-vehicle"]) do
+	for _, resistance in pairs (spidertron.resistances or {}) do
+		if resistance.type == "acid" then
+			table.insert(spidertron.resistances, {type = "toxic", decrease = resistance.decrease, percent = resistance.percent})
+		end
+	end
+end
+
+for name, spidertron in pairs(data.raw["wall"]) do
 	for _, resistance in pairs (spidertron.resistances or {}) do
 		if resistance.type == "acid" then
 			table.insert(spidertron.resistances, {type = "toxic", decrease = resistance.decrease, percent = resistance.percent})
